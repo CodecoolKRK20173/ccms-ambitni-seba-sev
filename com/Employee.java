ed278c11f9cb58429467654db25f355f87bc93f4
SebastianS <strycharz97@gmail.com>
2018-06-26 13:34:00 +0200

    Implement Employee class

--- com/Employee.java
+++ com/Employee.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
-public class Employee {
+public class Employee extends User{
+
+    private static ArrayList<User> employees = new ArrayList<>();
+
+    public Employee(String login, String password, String name, String contact){
+        super(login, password, name, contact);
+        employees.add(this);
+    }
+
+    public static ArrayList<User> getEmployees() {
+        return employees;
+    }
+
+    public static void removeEmployee(User employee){
+        employees.remove(employee);
+    }
 }
