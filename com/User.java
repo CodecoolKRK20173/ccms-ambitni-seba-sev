4fb3d96de3ea0a30aca620c82c5adf5e761bb241
Seweryn91 <s.talaj@gmail.com>
2018-06-26 11:30:48 +0200

    Add essential variables to User class

--- com/User.java
+++ com/User.java
@@ -1,6 +1,34 @@
 
-public class User {
-    public User(String name, String pass){
+public abstract class User {
+    String login;
+    String password;
+    String name;
+    String contact;
+    public User(String login, String password, String name, String contact){
+        this.login = login;
+        this.password = password;
+        this.name = name;
+        this.contact = contact;
+    }
 
+    public String getLogin(){
+        return login;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+
+    public String getContact(){
+        return contact;
+    }
+
+    public String toString(){
+        return "login: " + getLogin() + " pass: " + getPassword() + " name: " + getName() + " contact: " + getContact();
     }
 }
