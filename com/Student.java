e08a411dba116e645e6299f7e5735392c5e53d4e
SebastianS <strycharz97@gmail.com>
2018-06-26 13:35:41 +0200

    Implement Student class

--- com/Student.java
+++ com/Student.java
@@ -1,4 +1,52 @@
-package PACKAGE_NAME;
 
-public class Student {
+import java.util.ArrayList;
+
+public class Student extends User{
+    private static ArrayList<User> students = new ArrayList<>();
+    private ArrayList<Assignment> assignments = new ArrayList<>();
+    private boolean presence = false;
+
+    public Student(String login, String password, String name, String contact){
+        super(login, password, name, contact);
+        students.add(this);
+    }
+
+    public static void removeStudent(User student){
+        students.remove(student);
+    }
+
+    public static User getStudent(int index){
+        return students.get(index);
+    }
+
+    public static ArrayList<User> getStudents() {
+        return students;
+    }
+
+    public Assignment getAssignment(int index) {
+        return assignments.get(index);
+    }
+
+    public ArrayList<Assignment> getAssigments() {
+        return assignments;
+    }
+
+    public boolean isPresent() {
+        return presence;
+    }
+
+    public void setPresence(boolean presence) {
+        this.presence = presence;
+    }
+
+    public void addAssignment(Assignment a){
+        this.assignments.add(a);
+    }
+
+    public void doAssignment(int index){
+        this.assignments.get(index).doAssignment();
+    }
+
+
+
 }
