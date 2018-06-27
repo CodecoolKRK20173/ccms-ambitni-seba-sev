94f6910a3d7557d4cc40338609c944c749700541
SebastianS <strycharz97@gmail.com>
2018-06-28 00:28:06 +0200

    Add View class

--- com/View.java
+++ com/View.java
@@ -1,4 +1,58 @@
-package PACKAGE_NAME;
+import java.util.List;
 
 public class View {
+
+
+    private static String[] employeeList = {"Show Students"};
+    private static String[] studentList = {"Submit Assignment", "Show grades"};
+    private static String[] jerzyList = {"Show mentors", "Show Students", "Add Mentor", "Remove Mentor", "Edit Mentor"};
+    private static String[] mentorList = {"Show Students", "Add Student", "Remove Student", "Edit Student", "Add assignment"
+            ,"Grade Assignment", "Check Attendance"};
+
+
+
+    public static void printList(List<String> list){
+        int i = 1;
+        for(String s : list){
+            System.out.println(i + "." + s);
+            i++;
+        }
+    }
+
+
+    public static void printMenu(String name, String[] menuList){
+
+        System.out.println("Hello " + name);
+        System.out.println("What would you like to do:");
+        int i = 1;
+
+        for(String line : menuList){
+            System.out.println("    ("+ i +") " + line);
+            i++;
+        }
+
+
+    }
+
+    public static void print(Object o){
+        System.out.println(o);
+    }
+
+    public static void printEmployeeMenu(String name){
+        printMenu(name, employeeList);
+    }
+
+    public static void printStudentMenu(String name){
+        printMenu(name, studentList);
+    }
+
+    public static void printMentorMenu(String name){
+        printMenu(name, mentorList);
+    }
+
+
+    public static void printJerzyMenu(){
+        printMenu("Jerzy", jerzyList);
+    }
+
 }
