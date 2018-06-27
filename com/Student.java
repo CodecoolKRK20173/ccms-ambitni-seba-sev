54fb426ebd8b1753ab9ff3db833be1951b7a93e5
SebastianS <strycharz97@gmail.com>
2018-06-27 10:52:33 +0200

    Refactor Student class

--- com/Student.java
+++ com/Student.java
@@ -5,10 +5,12 @@ public class Student extends User{
     private static ArrayList<User> students = new ArrayList<>();
     private ArrayList<Assignment> assignments = new ArrayList<>();
     private boolean presence = false;
+    private String classroom;
 
     public Student(String login, String password, String name, String contact){
         super(login, password, name, contact);
         students.add(this);
+
     }
 
     public static void removeStudent(User student){
@@ -41,12 +43,18 @@ public class Student extends User{
 
     public void addAssignment(Assignment a){
         this.assignments.add(a);
+
     }
 
-    public void doAssignment(int index){
-        this.assignments.get(index).doAssignment();
+    public void setClassroom(String classroom) {
+        this.classroom = classroom;
     }
 
+    public String getClassroom() {
+        return classroom;
+    }
 
-
+    public String toCSV(){
+        return super.toCSV() + this.classroom;
+    }
 }
