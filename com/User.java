a541e2a4546646f8d44d377a9f6ddaf950561b3f
SebastianS <strycharz97@gmail.com>
2018-06-28 00:27:44 +0200

    Refactor User class

--- com/User.java
+++ com/User.java
@@ -5,38 +5,39 @@ public abstract class User {
     private String name;
     private String contact;
 
-    public User(String login, String password, String name, String contact){
+    public User(String login, String password, String name, String contact) {
         this.login = login;
         this.password = password;
         this.name = name;
         this.contact = contact;
     }
 
-    public String getLogin(){
+    public String getLogin() {
         return login;
     }
 
-    public String getPassword(){
+    public String getPassword() {
         return password;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
 
-    public String getContact(){
+    public String getContact() {
         return contact;
     }
 
-    public String toString(){
+    public String toString() {
         return "login: " + getLogin() + " pass: " + getPassword() + " name: " + getName() + " contact: " + getContact();
     }
 
-    public String toCSV(){
-        return this.getClass().getSimpleName()+ "," + name + "," + login + "," + password + "," + contact + ", ";
+    public String toCSV() {
+        return this.getClass().getSimpleName() + "," + name + "," + login + "," + password + "," + contact + ", ";
     }
 
-    public void setClassroom(String classroom){}
+    public void setClassroom(String classroom) {
+    }
 
 }
